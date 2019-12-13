efda20ab800f0e49405b6fa9bf3be80f4cfa5918
ShunOmine <shun12baseball@gmail.com>
2019-12-13 19:46:38 +0900

    create hooks,

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,77 +1,56 @@
-import React, { Component } from 'react'
+import React, { FC, useEffect } from 'react'
 import _ from 'lodash'
 // firebase
 import 'firebase/auth'
 // models
 import { MainProps } from '../models'
-// actions
-import { addName } from '../actions/User'
-import { getMessages } from '../actions/Messages'
 // lib
-import { connect } from "react-redux"
 import moment from 'moment'
 import 'moment/locale/ja'
 // stylesheets
 import '../stylesheets/App.css'
+import {useApp} from "../hooks/useApp";
 
 export const Name = document.cookie.replace(/(?:(?:^|.*;\s*)name\s*=\s*([^;]*).*$)|^.*$/, "$1")
 
 interface Props extends MainProps {}
 
-class App extends Component<Props> {
-  constructor(props: Props) {
-    super(props)
-    this.onSubmit = this.onSubmit.bind(this)
-  }
-  state = {
-    name: ''
-  }
+const App: FC = (): any => {
+  // hooks
+  const { messages, getApp, onSubmit, setName } = useApp()
 
-  componentDidMount(): void {
-    this.props.getMessages()
-  }
+  // effect
+  useEffect(() => {
+    getApp()
+  }, [getApp])
 
-  onSubmit = () => {
-    this.props.addName(this.state.name)
-    document.cookie = `name=${this.state.name}; path=/;`
-  }
-
-  renderForm = () => {
+  const renderForm = () => {
     return Name ?
       <p>{Name}</p>
       :
-      <form onSubmit={() => this.onSubmit()}>
-        <input id="name" type="text" name="name" onChange={(e) => this.setState({ name: e.target.value })} placeholder="お名前を入力して下さい" />
+      <form onSubmit={() => onSubmit()}>
+        <input id="name" type="text" name="name" onChange={(e) => setName(e.target.value)} placeholder="お名前を入力して下さい" />
         <input type="submit" value="ログイン" />
       </form>
   }
 
-  renderTime = (time: any) => {
+  const renderTime = (time: any) => {
     moment.locale("ja")
     return moment(time).fromNow()
   }
 
-  render() {
-    const { messages } = this.props
-    return (
-      <div className="App">
-        { this.renderForm() }
-        {_.map(messages, (message, index) => (
-          <div key={index} className="messages_area">
-            <p className="name">{message.name}</p>
-            <p className="content">{message.content}</p>
-            <p className="time">{this.renderTime(message.created_at.toDate())}</p>
-          </div>
-        ))}
-      </div>
-    )
-  }
+  return (
+    <div className="App">
+      { renderForm() }
+      {_.map(messages, (message, index) => (
+        <div key={index} className="messages_area">
+          <p className="name">{message.name}</p>
+          <p className="content">{message.content}</p>
+          <p className="time">{renderTime(message.created_at.toDate())}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
-const mapStateToProps = (state: any) => ({
-  name: state.name,
-  messages: state.messages
-})
-const mapDispatchToProps = ({ addName, getMessages })
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
---
+++ src/hooks/useApp.ts
@@ -0,0 +1,37 @@
+import { useCallback, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+// actions
+import { addName } from '../actions/User'
+import { getMessages } from '../actions/Messages'
+// models
+import { MainProps } from '../models'
+
+interface State extends MainProps{}
+
+export const useApp = () => {
+  // state
+  const [name, setName] = useState('')
+  // dispatch
+  const dispatch = useDispatch()
+  // select
+  const [ messages ] = useSelector(
+    (state: State) => [
+      state.messages
+    ]
+  )
+
+  const getApp = useCallback(async () => {
+    try {
+      dispatch(getMessages())
+    } catch (e) {
+      console.log(e)
+    }
+  }, [dispatch])
+
+  const onSubmit = () => {
+    dispatch(addName(name))
+    document.cookie = `name=${name}; path=/;`
+  }
+
+  return { messages, getApp, onSubmit, setName }
+}
